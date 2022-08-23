0dd256d0bd0807871d4f20401029ac325d340f7e
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2022-08-23 14:48:02 +0100

    Created the react app: Github-temp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App ({gitTempArr}) {
+  const structure = gitTempArr.map((file) => {
+    return <div className='fileContainer' key={file.id}>
+      <div className='left-contents'>
+        {file.name === "file"? <img src={file.iconFile} alt="file icon" /> : <img src={file.iconFolder} alt="folder icon" />}
+        {file.commitName}
+      </div>
+      <div className='center-content'>
+        {file.commitMessage}
+      </div>
+      <div className='right-content'>
+        {file.time}
+      </div>
     </div>
-  );
+  })
+  return (
+    <div className='githubContainer'>
+      {structure}
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,117 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const fileIcon = require("./icons/file.png");
+const folderIcon = require("./icons/folder.png");
+
+const githubTempArray = [
+  {
+    id: 1,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: "build",
+    name: "folder",
+    commitMessage: "close #3654, Replace es3ify with Babel ES3 transforms (#1688)",
+    time: ""
+  },
+  {
+    id: 2,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: "docs",
+    name: "folder",
+    commitMessage: "Mention that we're observable in the API",
+    time: ""
+  },
+  {
+    id: 3,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: "examples",
+    name: "folder",
+    commitMessage: "Update docs to use test with enzyme (#1786)",
+    time: ""
+  },
+  {
+    id: 4,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: "logo",
+    name: "folder",
+    commitMessage: "Use Redux logo as favicon on GitBook docs (#1761)",
+    time: ""
+  },
+  {
+    id: 5,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: "src",
+    name: "folder",
+    commitMessage: "only warn for unexpected key once (#1672)",
+    time: ""
+  },
+  {
+    id: 6,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: "test",
+    name: "file",
+    commitMessage: "test the PostgreSQL DB to ensure secure connection (#2233)",
+    time: ""
+  },
+  {
+    id: 7,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: ".babelrc",
+    name: "file",
+    commitMessage: "close #1687. Replace ES3lify with Babel",
+    time: ""
+  },
+  {
+    id: 8,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: ".editorconfig",
+    name: "file",
+    commitMessage: "editor configurations: Do not edit whitespaces",
+    time: ""
+  },
+  {
+    id: 9,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: ".eslintignore",
+    name: "file",
+    commitMessage: "ignore funny error messages",
+    time: ""
+  },
+  {
+    id: 10,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: ".eslintrc",
+    name: "file",
+    commitMessage: "updated ESlint extension on vsCode and changed some files",
+    time: ""
+  },
+  {
+    id: 11,
+    iconFile: fileIcon,
+    iconFolder: folderIcon,
+    commitName: ".flowconfig",
+    name: "file",
+    commitMessage: "Drink water and rest, let the configuration flow naturally",
+    time: ""
+  }
+]
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App gitTempArr={githubTempArray}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
